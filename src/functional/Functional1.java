7c32028d61db54d0d5618e23ebf16d87e75aace6
basavp <basav@outlook.in>
2022-10-05 19:52:02 +0530

    stream.filter.forEach

--- src/functional/Functional1.java
+++ src/functional/Functional1.java
@@ -5,7 +5,18 @@ import java.util.List;
 public class Functional1 {
     //print list of numbers using list of numbers using functional programming
     public static void main(String[] args){
-        printAllNumbersInList(List.of(12,12,13,13,141,14,1214,12445,767));
+        /*printAllNumbersInList(List.of(12,12,13,13,141,14,1214,12445,767));*/
+        printAllEvenNumbersInList(List.of(12,12,13,13,141,14,1214,12445,767));
+    }
+
+    private static boolean isEven(int integer){
+        return integer%2 ==0 ;
+    }
+
+    private static void printAllEvenNumbersInList(List<Integer> integers) {
+        integers.stream().
+                filter(Functional1::isEven).   //checking whether the element is even
+                forEach(System.out::println); //method reference
 
     }
 
@@ -19,6 +30,10 @@ public class Functional1 {
         integers.stream().forEach
                 (Functional1::print); //method reference
 
+        System.out.println();
+
+        integers.stream().forEach
+                (System.out::println); //method reference
 
     }
 
